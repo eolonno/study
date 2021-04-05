aee1195c27fe39d20cc99f686d8117be47108819
QWSTI <yegor.l@mail.ru>
2021-04-05 10:30:24 +0300

    05/04/2021

--- ООП/Четвертый семестр/ЛР №6-7/ЛР №6-7/Saver.cs
+++ ООП/Четвертый семестр/ЛР №6-7/ЛР №6-7/Saver.cs
@@ -9,8 +9,15 @@ using System.Xml.Serialization;
 
 namespace ЛР__6_7
 {
+    /// <summary>
+    /// Класс для сохрания данных о жильцах в документ
+    /// </summary>
     public static class Saver
     {
+        /// <summary>
+        /// Сохранение в документ
+        /// </summary>
+        /// <param name="tenants">Список для сохранения</param>
         public static void SaveList(List<Tenant> tenants)
         {
             XmlSerializer xml = new XmlSerializer(typeof(List<Tenant>));
@@ -19,6 +26,10 @@ namespace ЛР__6_7
                 xml.Serialize(fs, tenants);
             }
         }
+        /// <summary>
+        /// Чтение из документа
+        /// </summary>
+        /// <returns>Список элементов класса Tenant</returns>
         public static List<Tenant> ReadList()
         {
             try
